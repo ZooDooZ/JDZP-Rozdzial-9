ebc082188e5953e5e7e816430801e8804b5fd928
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-24 14:42:56 +0200

    Łańcuch znaków czytany wspak

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,22 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Podaj wyraz który chcesz zobaczyć od tyłu: ");
+        String word = keyboard.nextLine();
+        reverseWord(word);
 
+    }
+    static String reverseWord(String word) {
+
+        char[] array = word.toCharArray();
+        for (int i = array.length; i > 0; i--) {
+            System.out.print(array[i-1]);
+        }
+        return String.valueOf(array);
     }
 }
