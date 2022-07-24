1807a12f38ff2a339ba6a17d6fbc5777727da2bf
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-24 14:40:58 +0200

    Licznik słów

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,27 @@
 package org.zoodooz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Wpisz dowolone zdanie aby dowiedzieć się ile zawiera słów: ");
+        String sentence = keyboard.nextLine();
+        System.out.println("W podanym zdaniu jest: " + getNumberOfLetter(sentence) + " słów.");
+
+
+    }
+    static int getNumberOfLetter(String str){
+        char[] array;
+        int numberOfLetter = 0;
+
+        array = str.toCharArray();
+        for(int i = 0; i < array.length; i++) {
+            if (Character.isWhitespace(array[i])){
+                numberOfLetter++;
+            }
+        }
+        return numberOfLetter+1;
     }
 }
