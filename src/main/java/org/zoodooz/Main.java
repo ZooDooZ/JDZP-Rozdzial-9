778ace3be84b2c43d8485183eedf3ef67814f04f
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-24 14:44:39 +0200

    Łańcuch znaków czytany wspak

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Podaj wyraz który chcesz zobaczyć od tyłu: ");
         String word = keyboard.nextLine();
