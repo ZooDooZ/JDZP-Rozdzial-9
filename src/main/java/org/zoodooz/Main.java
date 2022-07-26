11f52f619b3f30fe878c732ede4ca1a31b209a5f
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-26 09:37:16 +0200

    wstępne założenia- do poprawy

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,23 @@
 package org.zoodooz;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Podaj nazwe pliku (po nazwie dodaj .txt):");
+        String name = keyboard.nextLine();
+
+        File file = new File(name);
+        Scanner openFile = new Scanner(file);
+
+        Counter counter = new Counter(openFile);
+
+
+        System.out.println("Liczba słów w podanym pliku wynosi: " + counter.getNumberOfWords());
 
     }
 }
