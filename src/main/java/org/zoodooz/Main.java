8cd89c66a6b72f18d992f9e93fa06d0f1b14851c
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-29 15:31:39 +0200

    Statystyki dotyczące loterii

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,7 +1,25 @@
 package org.zoodooz;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("pbnumbers.txt");
+        Scanner scanner = new Scanner(file);
+
+        while(scanner.hasNext()){
+//            System.out.println(scanner.nextLine());
+            String[] tokens = scanner.nextLine().split("");
+
+            for(int i = 0; i < tokens.length; i++){
+                System.out.println(tokens[i]);
+            }
+            System.out.println();
+        }
 
     }
 }
---
+++ src/main/java/org/zoodooz/Statistic.java
@@ -0,0 +1,19 @@
+package org.zoodooz;
+
+public class Statistic {
+
+//    public String getTenMostOftenDrawsNumber(String[] tokens){
+//
+//        int counter = 0;
+//
+//        for(int i = 0; i < tokens.length; i++){
+//            for(int y = 0; y < tokens.length; y++){
+//                if(tokens[i].equalsIgnoreCase(tokens[y])){
+//                    counter++;
+//                    if (counter > )
+//                }
+//            }
+//        }
+//        System.out.println();
+//    }
+}
