6016450e10f7fa314be0895b347cd65147127f04
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-08-02 11:58:52 +0200

    Ceny benzyny

--- src/main/java/org/zoodooz/Calculation.java
+++ src/main/java/org/zoodooz/Calculation.java
@@ -3,12 +3,15 @@ package org.zoodooz;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculation {
 
-    private String[] tokens;
-    private ArrayList<Double> petrolNumber = new ArrayList<>();
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private double lowestPrice;
+    private double highestPrice;
+
     int counter = 0;
 
     public Calculation() throws FileNotFoundException {
@@ -16,21 +19,81 @@ public class Calculation {
         Scanner scanner = new Scanner(file);
 
         while (scanner.hasNext()) {
-            tokens = scanner.nextLine().split("[-:]");
+            String[] tokens = scanner.nextLine().split("[-:]");
             for (int i = 0; i < tokens.length; i++){
-                petrolNumber.set(Double.parseDouble(tokens[i]));
+                arrayList.add(i,tokens[i]);
             }
-
-
         }
     }
 
-    public double getAvgYearPrice(){
+    public double getAvgYearPrice(int year){
+        String str = String.valueOf(year);
         double avgYearPrice = 0;
-        for(int i = 0; i < tokens.length; i++){
-            System.out.println(tokens[i]);
+        int counter = 0;
+        for(int i = 0; i < arrayList.size(); i++){
+            if(arrayList.get(i).equals(str)){
+                counter++;
+                avgYearPrice += Double.parseDouble(arrayList.get(i+1));
+            }
         }
-
+        avgYearPrice = avgYearPrice /counter;
         return avgYearPrice;
     }
+
+    public double getAvgMonthPrice(int month){
+        double avgMonthPrice = 0;
+        int counter = 0;
+        for (int i = 0; i < arrayList.size(); i+=4){
+            if(Integer.parseInt(arrayList.get(i)) == month){
+                counter++;
+                avgMonthPrice += Double.parseDouble(arrayList.get(i+3));
+            }
+        }
+        avgMonthPrice = avgMonthPrice/counter;
+        return avgMonthPrice;
+    }
+
+    public void getLowestPricePerYear(int year){
+        String str = String.valueOf(year);
+        lowestPrice = Double.MAX_VALUE;
+        String data = "";
+        for(int i = 0; i < arrayList.size(); i++){
+            if(arrayList.get(i).equals(str)){
+                if(Double.parseDouble(arrayList.get(i+1)) < lowestPrice){
+                    lowestPrice = Double.parseDouble(arrayList.get(i+1));
+                    data = arrayList.get(i-2) + " " + arrayList.get(i-1) +" "+ arrayList.get(i);
+                }
+            }
+        }
+        System.out.println(data + "- cena wynosiła: " + lowestPrice + " zł");
+    }
+
+    public void getHighestPricePerYear(int year){
+        String str = String.valueOf(year);
+        highestPrice = Double.MIN_VALUE;
+        String data = "";
+        for(int i = 0; i < arrayList.size(); i++){
+            if(arrayList.get(i).equals(str)){
+                if(Double.parseDouble(arrayList.get(i+1)) > highestPrice){
+                    highestPrice = Double.parseDouble(arrayList.get(i+1));
+                    data = arrayList.get(i-2) + " " + arrayList.get(i-1) +" "+ arrayList.get(i);
+                }
+            }
+        }
+        System.out.println(data + "- cena wynosiła: " + highestPrice + " zł");
+    }
+
+    public void getPriceFromLowToHigh(){
+        ArrayList<Double> arrayD = new ArrayList<>();
+        int counter = 0;
+        for(int i = 4; i < arrayList.size(); i+=4){
+            counter++;
+            arrayD.add(counter,Double.parseDouble(arrayList.get(i)));
+        }
+        for(int i = 0; i<arrayD.size(); i++){
+            System.out.println(arrayD.get(i));
+        }
+    }
+
+
 }
--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -8,7 +8,30 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         Calculation calculation = new Calculation();
-        calculation.getAvgYearPrice();
 
+        System.out.println("_________ŚREDNIA CENA BENZYNY W LATACH 1993-2013_________ ");
+        for(int i = 0; i < 21; i++){
+            System.out.printf("W rok " + (1993+i) + " wynosiła: %.3f zł\n",calculation.getAvgYearPrice(1993+i));
+        }
+        System.out.println();
+
+        System.out.println("_________ŚREDNIA MIESIECZNA CENA BENZYNY W LATACH 1993-2013_________ ");
+        for(int i = 1; i < 13 ;i++){
+            System.out.printf("W miesiącu nr: " + i + " wynosiła: %.3f zł\n",calculation.getAvgMonthPrice(i) );
+        }
+        System.out.println();
+
+        System.out.println("_________NAJNIŻSZA CENA BENZYNY W LATACH 1993-2013_________ ");
+        for(int i = 0; i < 21 ;i++){
+            calculation.getLowestPricePerYear(1993+i);
+        }
+        System.out.println();
+
+        System.out.println("_________NAJWYŻSZA CENA BENZYNY W LATACH 1993-2013_________ ");
+        for(int i = 0; i < 21 ;i++){
+            calculation.getHighestPricePerYear(1993+i);
+        }
+
+        calculation.getPriceFromLowToHigh();
     }
 }
